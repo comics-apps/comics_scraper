c01923c3632e46071bcbd560920f8d8fb1b514b7
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-03-10 15:28:29 +0100

    Add basic app structure

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'dotenv'
+gem 'dry-system'
 gem 'rspec'
 gem 'rubocop'
---
+++ system/container.rb
@@ -0,0 +1,9 @@
+require 'dry/system/container'
+
+class ComicsScraper < Dry::System::Container
+  configure do |config|
+    config.auto_register = %w(lib)
+  end
+
+  load_paths!('lib')
+end
